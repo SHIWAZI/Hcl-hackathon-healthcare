bf6bfc8c9bbd70802bb14c0aff552011fbe9190a
dhanan-doodle <120489293+dhanan-doodle@users.noreply.github.com>
2025-11-22 14:43:03 +0530

    routes changes

--- frontend/src/routes/ServiceProvider/Goal.jsx
+++ frontend/src/routes/ServiceProvider/Goal.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { Outlet } from 'react-router'
 
-export default function Home() {
+export default function Goal() {
     return (
-        <div>Home
-            <Outlet />
+        <div>
+
         </div>
     )
 }
--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -2,13 +2,24 @@ import {
     createBrowserRouter,
 } from "react-router";
 import Home from "./Home";
+import Goal from "./ServiceProvider/Goal";
+import ServiceProviderLayout from "../Componnets/Layouts/ServiceProviderLayout";
 
 let router = createBrowserRouter([
     {
         path: "/",
         Component: Home,
-        // loader: loadRootData,
     },
+    {
+        path: "/service-provider",
+        Component: ServiceProviderLayout,
+        children: [
+            {
+                path: "/service-provider/:id/goal",
+                Component: Goal,
+            },
+        ],
+    }
 ]);
 
 export default router;
