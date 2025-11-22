a50792f03cc5d50821092777177e7535c4fa33ba
dhanan-doodle <120489293+dhanan-doodle@users.noreply.github.com>
2025-11-22 14:54:02 +0530

    Add redux store

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,11 +4,15 @@ import {
 } from "react-router";
 import './App.css'
 import router from './routes';
-
+import store from './store'
+import { Provider } from 'react-redux'
 function App() {
 
   return (
-    <RouterProvider router={router} />
+    <Provider store={store}>
+
+      <RouterProvider router={router} />
+    </Provider>
   )
 }
 
